056c47f49641975f712de83fc78673d5cc6ff457
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2025-12-11 16:59:28 -0500

    draw grid

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -2,6 +2,12 @@ const LIGHT_GREEN = "#aad751" // google light green
 const DARK_GREEN = "#a2d149" // google dark green
 const OUTLINE_GREEN = "#578a34"
 const MENU_GREEN = "#4a752c"
+
+const BOARD_HEIGHT = 15;
+const BOARD_WIDTH = 17;
+
+const START_X = 3; // zero-indexed
+const START_Y = 7; // zero-indexed
 export class Renderer {
   constructor(
     private ctx: CanvasRenderingContext2D,
@@ -16,8 +22,25 @@ export class Renderer {
 
     // tiled board
     const margin = 23;
-    this.ctx.fillStyle = LIGHT_GREEN;
-    this.ctx.fillRect(margin, margin, this.width - 2 * margin, this.height - 2 * margin);
+
+    const grid_height = this.height - 2 * margin;
+    const grid_width = this.width - 2 * margin;
+
+    const cell_height = (grid_height) / BOARD_HEIGHT;
+    const cell_width = (grid_width) / BOARD_WIDTH;
+
+    let isLightGreen = true;
+
+    // draw grid
+    for (let row = 0; row < BOARD_HEIGHT; row++) {
+      for (let col = 0; col < BOARD_WIDTH; col++) {
+        this.ctx.fillStyle = isLightGreen ? LIGHT_GREEN : DARK_GREEN;
+        const x = margin + col * cell_width;
+        const y = margin + row * cell_height;
+        this.ctx.fillRect(x, y, cell_width, cell_height);
+        isLightGreen = !isLightGreen;
+      }
+    }
   }
 
   clear() {
